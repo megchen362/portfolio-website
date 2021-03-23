abc106bbd7d151fc74b5674fe65806f9640c909b
Your Name <you@example.com>
2021-03-23 17:13:36 -0500

    Created sticky nav with smooth scrolling for Dimes.html

--- js/script.js
+++ js/script.js
@@ -6,4 +6,27 @@ function menuToggle () {
   } else {
     x.className = 'navtoggle';
   }
-}
+};
+
+let mainNavLinks = document.querySelectorAll("nav ul.scroll_nav li a");
+let mainSections = document.querySelectorAll("main section");
+
+let lastId;
+let cur = [];
+
+window.addEventListener("scroll", event => {
+  let fromTop = window.scrollY;
+
+  mainNavLinks.forEach(link => {
+    let section = document.querySelector(link.hash);
+
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      link.classList.add("current");
+    } else {
+      link.classList.remove("current");
+    }
+  });
+});
